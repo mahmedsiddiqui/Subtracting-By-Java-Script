e323cdbd8aec43d9fb1272904c1b1193caa64f8f
Muhammad Ahmed Siddiqui <154745545+mahmedsiddiqui@users.noreply.github.com>
2024-02-29 11:24:58 +0500

    Add files via upload

---
+++ subtracting in javascript/app.js
@@ -0,0 +1,4 @@
+const num1 = parseInt(prompt('Enter Your First Number for Subtracting'));
+const num2 = parseInt(prompt('Enter Your Second Number for Subtracting '));
+const sum = num1 - num2;
+document.write(`Your Sum is ${sum}`); 
